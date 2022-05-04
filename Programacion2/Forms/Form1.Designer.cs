5ac999bf1395167b733f51d255c6c621f19c024e
yaderxd <yaderoviedo4@gmail.com>
2022-05-04 10:18:34 -0600

    cambio

--- Programacion2/Forms/Form1.Designer.cs
+++ Programacion2/Forms/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Programacion2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnAceptar = new System.Windows.Forms.Button();
             this.cboTiempo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
@@ -55,6 +56,8 @@ namespace Programacion2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.cboTiempo);
             this.Controls.Add(this.btnAceptar);
