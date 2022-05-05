18544b1b027e208a92f8ace3eeeacfd293004a7a
yaderxd <yaderoviedo4@gmail.com>
2022-05-05 09:11:39 -0600

    cambio

--- Programacion2/Forms/Form1.Designer.cs
+++ Programacion2/Forms/Form1.Designer.cs
@@ -32,11 +32,14 @@ namespace Programacion2
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnAceptar = new System.Windows.Forms.Button();
             this.cboTiempo = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(673, 10);
+            this.btnAceptar.Location = new System.Drawing.Point(622, 16);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(115, 23);
             this.btnAceptar.TabIndex = 0;
@@ -47,10 +50,26 @@ namespace Programacion2
             // cboTiempo
             // 
             this.cboTiempo.FormattingEnabled = true;
-            this.cboTiempo.Location = new System.Drawing.Point(12, 12);
+            this.cboTiempo.Location = new System.Drawing.Point(3, 16);
             this.cboTiempo.Name = "cboTiempo";
             this.cboTiempo.Size = new System.Drawing.Size(531, 21);
             this.cboTiempo.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 78);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 360);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cboTiempo);
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 50);
+            this.panel1.TabIndex = 3;
             // 
             // Form1
             // 
@@ -59,11 +78,12 @@ namespace Programacion2
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cboTiempo);
-            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -72,6 +92,8 @@ namespace Programacion2
 
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.ComboBox cboTiempo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
